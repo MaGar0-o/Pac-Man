79445677f834e411c33d1b64d598210828469cae
MaGaroo <amoomajid99@gmail.com>
2017-11-25 18:31:25 +0330

    add structs Game, Time, Map, Ghost, Man

--- main.c
+++ main.c
@@ -4,6 +4,38 @@
 
 #include <stdio.h>
 
+struct Man {
+    int x, y;
+    int direction;
+    int health;
+};
+
+struct Ghost {
+    int x, y;
+    int state;
+    int remainingDefenseTime;
+    int direction;
+};
+
+struct Map {
+    int n, m;
+    char **cells;
+};
+
+struct Time {
+    int minutes, seconds;
+};
+
+struct Game {
+    int score;
+    Man *pacman;
+    Ghost *blinky;
+    Ghost *pinky;
+    Ghost *clyde;
+    Ghost *inky;
+    Map *map;
+};
+
 int main() {
 
     return 0;
