88dc61c1a31a37ff21e2987e95ebdaf8a9c330a4
MaGaroo <amoomajid99@gmail.com>
2017-12-17 22:02:31 +0330

    initiate game

--- src/game.c
+++ src/game.c
@@ -2,6 +2,20 @@
 #include "map.h"
 #include <stdio.h>
 
+void get_ghost(FILE *input, Ghost *ghost) {
+    int tmp1, tmp2;
+    fscanf(input, "%*s %d %d ", &tmp1, &tmp2);
+    ghost->dir = (Direction) tmp1;
+    //printf("%d %d\n", tmp1, tmp2);
+    //fscanf(input, "%d", tmp1);
+    ghost->blue = (bool) tmp2;
+    if (!tmp2)
+        fscanf(input, "%d ", &tmp1);
+    ghost->blueCounterDown = (unsigned long long int) tmp1;
+    fscanf(input, "(%d,%d) (%lf,%lf)", &ghost->startY, &ghost->startX, &ghost->y, &ghost->x);
+    //printf("%d %d %d %d", ghost->startX, ghost->startY, (int)ghost->x, (int)ghost->y);
+}
+
 void initiateGame(char *filename, Map *outMap, Game *outGame, Pacman *outPacman, Ghost *outGhosts) {
     FILE *input = fopen(filename, "r");
     printf("%s\n", filename);
@@ -34,10 +48,15 @@ void initiateGame(char *filename, Map *outMap, Game *outGame, Pacman *outPacman,
     int tmp;
     fscanf(input, "pacman: %lf %d (%d,%d) (%lf,%lf)\n", &outPacman->speed, &tmp, &outPacman->startX, &outPacman->startY,
            &outPacman->x, &outPacman->y);
-    printf("%d %d %d %d\n", outPacman->startX, outPacman->startY, (int)outPacman->x, (int)outPacman->y);
+    printf("%d %d %d %d\n", outPacman->startX, outPacman->startY, (int) outPacman->x, (int) outPacman->y);
     outPacman->dir = (Direction) tmp;
-    fscanf(input, "blinky: %*d %*d (%d,%d) (%lf,%lf)", &outGhosts[0].startX, &outGhosts[0].startY, &outGhosts[0].x,
-           &outGhosts[0].y);
+
+    for (int i = 0; i < 4; i++)
+        get_ghost(input, outGhosts + i);
+    outGhosts[0].type = BLINKY;
+    outGhosts[1].type = PINKY;
+    outGhosts[2].type = CLYDE;
+    outGhosts[3].type = INKY;
 }
 
 void checkEatables(Map *map, Game *outGame, Pacman *outPacman, Ghost *outGhosts) {
