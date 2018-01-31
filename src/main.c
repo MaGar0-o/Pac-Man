3ceb016b73f332f7764ae15f1a1a8ac2982b848c
MaGaroo <amoomajid99@gmail.com>
2018-01-31 17:23:03 +0330

    initiate game in main

--- src/main.c
+++ src/main.c
@@ -2,6 +2,15 @@
 // Created by MaGaroo on 1/31/18.
 //
 
+#include "game.h"
+
 int main() {
+    Map map;
+    Game game;
+    Pacman pacman;
+    Ghost ghosts[MAX_GHOST_COUNT];
+    initiateGame("res/map.txt", &map, &game, &pacman, ghosts);
+
+
     return 0;
 }
